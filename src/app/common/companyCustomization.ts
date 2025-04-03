a2ea9015354b7ef932c94fc71cb8f759710d5dcc
hrishitj <hrishitj@gmail.com>
2025-04-04 01:07:15 +0530

    homepage fonts and text

--- src/app/common/companyCustomization.ts
+++ src/app/common/companyCustomization.ts
@@ -1,4 +1,4 @@
 export const companySettings: Record<string, { theme: string; multiplier: number; header: {img: string, description: string []} }> = {
-    diamondWish: { theme: "#235171", multiplier: 1.5, header: {img: 'assets/home/diamondWish.jpg', description: ['From your heart, to your neck', 'From your heart, to your wrists']}},
-    default: { theme: "#000000", multiplier: 1, header: {img: '', description: ['From your heart, to your neck', 'From your heart, to your wrists']} }
+    diamondWish: { theme: "#235171", multiplier: 1.5, header: {img: 'assets/home/diamondWish.jpg', description: ['Make it personal', 'Make it memorable']}},
+    default: { theme: "#000000", multiplier: 1, header: {img: '', description: ['Make it personal', 'Make it memorable']} }
 };
