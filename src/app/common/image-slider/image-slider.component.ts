25b8ae8fa18ba8ab4276efe6fbcd5ac5d981b448
hrishitj <hrishitj@gmail.com>
2025-06-24 00:07:49 +0530

    image slider using css

--- src/app/common/image-slider/image-slider.component.ts
+++ src/app/common/image-slider/image-slider.component.ts
@@ -1,41 +1,15 @@
-import { isPlatformBrowser } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, HostListener, inject, input, NgZone, OnDestroy, OnInit, PLATFORM_ID, signal, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input} from '@angular/core';
 
 @Component({
   selector: 'app-image-slider',
   imports: [],
   templateUrl: './image-slider.component.html',
-  styleUrl: './image-slider.component.scss'
+  styleUrl: './image-slider.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ImageSliderComponent implements AfterViewInit, OnDestroy {
+export class ImageSliderComponent {
   public mediaItems = input<string[]>([]);
   public themeColor = input<string>('#000000');
-  public isSmallView = signal<boolean>(false);
-  private platformId = inject(PLATFORM_ID);
-  private resizeListener!: () => void;
-
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.checkWindowWidth(); // Initial check
-      this.resizeListener = this.onResize.bind(this);
-      window.addEventListener('resize', this.resizeListener);
-    }
-  }
-
-  ngOnDestroy(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      window.removeEventListener('resize', this.resizeListener);
-    }
-  }
-
-  private onResize(): void {
-    this.checkWindowWidth();
-  }
-
-  private checkWindowWidth(): void {
-    this.isSmallView.set(window.innerWidth < 800);
-  }
-  
 
   public selectedIndex: number = 0;
 
